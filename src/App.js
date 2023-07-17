bd760af2fd50b5f57f100203a6e9519f6a8e140e
KenMoldez <carlo15.code12@gmail.com>
2023-07-17 17:29:19 +0800

    lots of stuffs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+import { Route, Routes } from "react-router-dom";
+import NavigationBar from "./Navbar";
+import AdminPage from "./Admin";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavigationBar />
+
+      {/* <TEST /> */}
+      <Routes>
+        <Route exact path="/Admin" element={<AdminPage />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Navbar.jsx
@@ -0,0 +1,72 @@
+import { Navbar, Nav, Container, Offcanvas, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "./NavBar.css";
+
+const NavigationBar = () => {
+  return (
+    <div className="Navbar">
+      <nav>
+        {["lg"].map((expand) => (
+          <Navbar
+            key={expand}
+            expand={expand}
+            className="bg-body-tertiary mt-3 header"
+          >
+            <Container>
+              <Navbar.Brand>LAKBAY - TIKANG</Navbar.Brand>
+              <Navbar.Toggle
+                aria-controls={`offcanvasNavbar-expand-${expand}`}
+              />
+              <Navbar.Offcanvas
+                id={`offcanvasNavbar-expand-${expand}`}
+                aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+                placement="end"
+              >
+                <Offcanvas.Header closeButton>
+                  <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                    Offcanvas
+                  </Offcanvas.Title>
+                </Offcanvas.Header>
+                <Offcanvas.Body>
+                  <Nav className="justify-content-end flex-grow-1 pe-3">
+                    <Nav.Link>
+                      <Link className="navbar-link" to="/">
+                        Home
+                      </Link>
+                    </Nav.Link>
+                    <Nav.Link>
+                      <Link className="navbar-link" to="/about">
+                        About
+                      </Link>
+                    </Nav.Link>
+                    <Nav.Link>
+                      <Link className="navbar-link" to="/destinations">
+                        Destinations
+                      </Link>
+                    </Nav.Link>
+                    <Nav.Link>
+                      <Link className="navbar-link" to="/Admin">
+                        Admin
+                      </Link>
+                    </Nav.Link>
+                  </Nav>
+                  <Nav>
+                    <Button>
+                      <Nav.Link>
+                        <Link className="header-btn-group" to="/login">
+                          Login
+                        </Link>
+                      </Nav.Link>
+                    </Button>
+                  </Nav>
+                </Offcanvas.Body>
+              </Navbar.Offcanvas>
+            </Container>
+          </Navbar>
+        ))}
+      </nav>
+    </div>
+  );
+};
+
+export default NavigationBar;
